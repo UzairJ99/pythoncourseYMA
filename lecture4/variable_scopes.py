7fcbce05fb8961530fde042c578dd8bfcb65a804
Uzair Jawaid <uzair-jawaid@hotmail.com>
2020-07-18 22:49:30 -0400

    fixed error

--- lecture4/variable_scopes.py
+++ lecture4/variable_scopes.py
@@ -5,13 +5,17 @@ def squareNumber(n):
 
 
 users = 4  # this variable is out of main's scope
-bananas = 5  # this variable is also out of main's scope
+
+def triple(n):
+    bananas = 5  # this variable is also out of main's scope
+    bananas *= 3
+    print(bananas)
 
 
 def main():
     users = 3
     square_users = squareNumber(users)
-    #  print(bananas) uncomment this and you'll get an error. The variable bananas doesn't exist in this scope
+    print(bananas)  #  uncomment this and you'll get an error. The variable bananas doesn't exist in this scope
     print(square_users)  # the users variable from the main method scope is used
 
 
