5f88a934262451996181056bc06f627a669538c1
Uzair Jawaid <uzair-jawaid@hotmail.com>
2020-07-18 22:47:32 -0400

    added lecture 4 scope guideline

---
+++ lecture4/variable_scopes.py
@@ -0,0 +1,19 @@
+# function and variable scopes
+
+def squareNumber(n):
+    return n**2
+
+
+users = 4  # this variable is out of main's scope
+bananas = 5  # this variable is also out of main's scope
+
+
+def main():
+    users = 3
+    square_users = squareNumber(users)
+    #  print(bananas) uncomment this and you'll get an error. The variable bananas doesn't exist in this scope
+    print(square_users)  # the users variable from the main method scope is used
+
+
+if __name__ == "__main__":
+    main()
